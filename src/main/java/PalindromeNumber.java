c3d8131e061f1f61ab4ab7f746651e08723db214
SDussanov <dussanov.s@gmail.com>
2023-01-14 23:36:30 +0600

    1.0.0 first commit

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = -121;
+        int z = 10;
+
+        System.out.println(isPalindrome4(x));
+        System.out.println(isPalindrome4(y));
+        System.out.println(isPalindrome4(z));
+
+    }
+
+
+    public static boolean isPalindrome1(int x) {
+        String stringValue = Integer.toString(x);
+        char[] charArray = stringValue.toCharArray();
+        System.out.println(Arrays.toString(charArray));
+        int j = charArray.length - 1;
+        for (char c : charArray) {
+            if (c == charArray[j]) {
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean isPalindrome2(int x) {
+        if (x >= 0) {
+            ArrayList<Integer> stack = new ArrayList<>();
+            while (x > 0) {
+                stack.add(x % 10);
+                x = x / 10;
+            }
+
+            int j = stack.size() - 1;
+            for (int c : stack) {
+                if (c == stack.get(j)) {
+                    j--;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean isPalindrome3(int x) {
+        String original = Integer.toString(x);
+        String reverse = new StringBuilder(original).reverse().toString();
+        return original.equals(reverse);
+    }
+
+
+    public static boolean isPalindrome4(int x) {
+        if (x >= 0) {
+            int originalNumber = x;
+            int reverse = 0;
+            while (x != 0) {
+                int lastDigit = x % 10;
+                reverse = (reverse * 10) + lastDigit;
+                x /= 10;
+            }
+            return originalNumber == reverse;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+        int[] numsA = new int[]{2, 7, 11, 15};
+        int[] numsB = new int[]{3, 2, 4};
+        int[] numsC = new int[]{3, 3};
+        int[] numsD = new int[]{1, 1, 1, 1};
+
+        System.out.println(Arrays.toString(twoSum2(numsA, 9)));
+        System.out.println(Arrays.toString(twoSum2(numsB, 6)));
+        System.out.println(Arrays.toString(twoSum1(numsC, 6)));
+        System.out.println(Arrays.toString(twoSum1(numsD, 2)));
+    }
+
+
+    private static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+
+    private static int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+
+}
+
