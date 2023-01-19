a11b8831cc7b96b432a831fb18f4a7039ebdb57e
SDussanov <dussanov.s@gmail.com>
2023-01-19 23:39:31 +0600

    1.0.5 added solution remove duplicates sorted array

--- src/main/java/MergeTwoSortedLists.java
+++ src/main/java/MergeTwoSortedLists.java
@@ -1,16 +1,57 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class MergeTwoSortedLists {
     public static void main(String[] args) {
 
 
-
     }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists1(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists1(list1, list2.next);
+            return list2;
+        }
+    }
 
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = list1 == null ? list2 : list1;
+        return dummy.next;
+    }
+
+    public ListNode mergeTwoLists3(ListNode list1, ListNode list2) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (list1 != null) {
+            pq.add(list1.val);
+            list1 = list1.next;
+        }
+        while (list2 != null) {
+            pq.add(list2.val);
+            list2 = list2.next;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (!pq.isEmpty()) {
+            curr.next = new ListNode(pq.poll());
+            curr = curr.next;
+        }
+        return dummy.next;
     }
 
 
