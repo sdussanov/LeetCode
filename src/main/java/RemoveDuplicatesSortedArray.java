0052da2b2763cfdc3baccfd6b868abc66656f542
SDussanov <dussanov.s@gmail.com>
2023-01-20 23:29:01 +0600

    1.0.6 added solution remove element solution

--- src/main/java/RemoveDuplicatesSortedArray.java
+++ src/main/java/RemoveDuplicatesSortedArray.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class RemoveDuplicatesSortedArray {
-    Test
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    static int removeDuplicates(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : nums) if(!list.contains(i)) list.add(i);
+        int i = 0;
+        for(int j : list) nums[i++] = j;
+        return list.size();
+    }
 }
---
+++ src/main/java/RemoveElement.java
@@ -0,0 +1,32 @@
+public class RemoveElement {
+    public static void main(String[] args) {
+
+
+    }
+
+    static int removeElement1(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+
+
+    static int removeElement2(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
