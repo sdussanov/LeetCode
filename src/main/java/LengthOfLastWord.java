ee1c64b693c2ecbe0a3c113fc55120b3ec2956f7
SDussanov <dussanov.s@gmail.com>
2023-01-22 14:23:59 +0600

    1.0.9 added solution length of last word

--- src/main/java/LengthOfLastWord.java
+++ src/main/java/LengthOfLastWord.java
@@ -5,31 +5,31 @@ public class LengthOfLastWord {
         String s2 = "   fly me   to   the moon  ";
         String s3 = "s";
 
-        System.out.println(lengthOfLastWord2(s1));
-        System.out.println(lengthOfLastWord2(s2));
-        System.out.println(lengthOfLastWord2(s3));
+        System.out.println(lengthOfLastWord4(s1));
+        System.out.println(lengthOfLastWord4(s2));
+        System.out.println(lengthOfLastWord4(s3));
     }
 
 
-    static int lengthOfLastWord1(String s){
-        String lastWord = s.trim().substring(s.trim().lastIndexOf(" ")+1);
+    static int lengthOfLastWord1(String s) {
+        String lastWord = s.trim().substring(s.trim().lastIndexOf(" ") + 1);
         return lastWord.length();
     }
 
 
-    static int lengthOfLastWord2(String s){
-        return s.trim().substring(s.trim().lastIndexOf(" ")+1).length();
+    static int lengthOfLastWord2(String s) {
+        return s.trim().substring(s.trim().lastIndexOf(" ") + 1).length();
     }
 
 
-    public int lengthOfLastWord3(String s) {
+    static int lengthOfLastWord3(String s) {
         s = s.trim();
         int lastIndex = s.lastIndexOf(" ");
         return s.length() - lastIndex - 1;
     }
 
 
-    public int lengthOfLastWord4(String s) {
+    static int lengthOfLastWord4(String s) {
         int len = s.length(), lastLength = 0;
         while (len > 0 && s.charAt(len - 1) == ' ') {
             len--;
@@ -40,4 +40,5 @@ public class LengthOfLastWord {
         }
         return lastLength;
     }
+
 }
