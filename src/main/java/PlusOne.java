84f12d7f7f8ace4bfec087d77e31e49fbd19c7de
SDussanov <dussanov.s@gmail.com>
2023-01-22 16:43:27 +0600

    1.0.10 added solution Plus One

---
+++ src/main/java/PlusOne.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public static void main(String[] args) {
+        int[] digits1 = new int[]{1, 2, 3};
+        int[] digits2 = new int[]{4, 3, 2, 1};
+        int[] digits3 = new int[]{9};
+        int[] digits4 = new int[]{5, 4, 9, 9};
+
+        System.out.println(Arrays.toString(plusOne3(digits1)));
+        System.out.println(Arrays.toString(plusOne3(digits2)));
+        System.out.println(Arrays.toString(plusOne3(digits3)));
+        System.out.println(Arrays.toString(plusOne3(digits4)));
+    }
+
+
+    static int[] plusOne1(int[] digits) {
+        int head = digits.length - 1;
+
+        while (((digits[head] + 1) / 10) > 0) {
+            digits[head--] = 0;
+            if (head == -1) {
+                return insertAnElementAtAGivenIndex(digits, 0, 1);
+            }
+        }
+        digits[head] += 1;
+        return digits;
+    }
+
+
+    static int[] insertAnElementAtAGivenIndex(final int[] srcArray, int index, int newElement) {
+        int[] destArray = new int[srcArray.length + 1];
+        int j = 0;
+        for (int i = 0; i < destArray.length - 1; i++) {
+
+            if (i == index) {
+                destArray[i] = newElement;
+            } else {
+                destArray[i] = srcArray[j];
+                j++;
+            }
+        }
+        return destArray;
+    }
+
+
+    static int[] plusOne2(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+
+
+    static int[] plusOne3(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
